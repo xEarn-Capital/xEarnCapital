8808a41193a1201eea15b346b620cd297990463c
xearn <xEarn.capital@gmail.com>
2020-10-14 20:12:04 +0200

    Updates

---
+++ static/js/13.376549b7.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpxearn.capital"]=this["webpackJsonpxearn.capital"]||[]).push([[13],{1720:function(a,p){}}]);
+//# sourceMappingURL=13.376549b7.chunk.js.map
---
+++ static/js/9.b25f620b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpxearn.capital"]=this["webpackJsonpxearn.capital"]||[]).push([[9],{1899:function(a,n){},1907:function(a,n){}}]);
+//# sourceMappingURL=9.b25f620b.chunk.js.map
